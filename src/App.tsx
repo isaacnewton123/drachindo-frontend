2ec76d7990045be7d4427f03c5d16b90393e11d9
isaac newton <hanifmaulana1997@gmail.com>
2025-10-19 22:57:38 +0700

    route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeContainer from "./components/homeContainer";
 import VideoContainer from "./components/videoContainer";
 
 function App() {
   return (
-    <>
-    <VideoContainer/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeContainer />} />
+        <Route path="/video" element={<VideoContainer />} />
+
+        {/* <-- todo --> */}
+        {/* <Route path="/video/:id" element={<VideoContainer/>} /> */}
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
